1c721f64d050074c1a19fce9800c744b034935d8
Martin Roos <kulminaator@gmail.com>
2019-01-05 15:08:19 +0200

    pulling work parameters together into maps

--- src/s3_kompressor/zip_writer.clj
+++ src/s3_kompressor/zip_writer.clj
@@ -52,22 +52,40 @@
 (defn internal-write-zip-from
   "Keeps on taking elements from channel and writes them to the zipfile.
   Tries to create splitted files if split-size is not nil."
-  [transport-channel split-size filename-threaded-base]
-  (loop [zip-output (ZipOutputStream. (clojure.java.io/output-stream (make-zip-name filename-threaded-base 0)))
-         file-to-add (async/<!! transport-channel)
-         written 0
-         next-roll split-size]
-    (if file-to-add
+  [settings filename-threaded-base]
+  (loop [workpoint {
+                     :zip-output (ZipOutputStream. (clojure.java.io/output-stream (make-zip-name filename-threaded-base 0)))
+                     :file-to-add (async/<!! (:transport-channel settings))
+                     :written 0
+                     :next-roll (:split-size settings)
+                     }]
+    (if (:file-to-add workpoint)
       (do
-        (println (str "Adding " (:name file-to-add) " at " written " next roll " next-roll))
-        (.putNextEntry zip-output (create-zip-entry (:name file-to-add) (:modified-at file-to-add)))
-        (with-open [instream (open-stream (:input-stream-fn file-to-add))]
-          (clojure.java.io/copy instream zip-output :buffer-size 32768))
-        (recur (roll-zip-if-needed zip-output filename-threaded-base (+ written (:size file-to-add)) next-roll)
-          (async/<!! transport-channel)
-          (+ written (:size file-to-add))
-          (increase-roll-if-needed (+ written (:size file-to-add)) next-roll split-size)))
-      (.close zip-output))
+        ; actual work
+        (println (str "Adding " (:name (:file-to-add workpoint))
+                      " at " (:written workpoint) " next roll " (:next-roll workpoint)))
+        (.putNextEntry (:zip-output workpoint) (create-zip-entry (:name (:file-to-add workpoint))
+                                                                 (:modified-at (:file-to-add workpoint))))
+        (with-open [instream (open-stream (:input-stream-fn (:file-to-add workpoint)))]
+          (clojure.java.io/copy instream (:zip-output workpoint) :buffer-size 32768))
+
+        ; recur block
+        (recur
+          (merge workpoint {
+                             :zip-output (roll-zip-if-needed (:zip-output workpoint) filename-threaded-base
+                                                             (+ (:written workpoint)
+                                                                (:size (:file-to-add workpoint)))
+                                                             (:next-roll workpoint))
+                             :file-to-add (async/<!! (:transport-channel settings))
+                             :written (+ (:written workpoint) (:size (:file-to-add workpoint)))
+                             :next-roll (increase-roll-if-needed
+                                         (+ (:written workpoint)
+                                            (:size (:file-to-add workpoint)))
+                                         (:next-roll workpoint)
+                                         (:split-size settings))
+                             })
+          ))
+      (.close (:zip-output workpoint)))
     ))
 
 (defn irange
@@ -78,8 +96,8 @@
 (defn dispatch-internal-zip-writers
   "Dispatches a worker thread for filename to write contents from transport-channel into it. Returns list of channels
   that contain the results of threads once they are done."
-  [transport-channel split-size filenames]
-  (mapv #(async/thread (internal-write-zip-from transport-channel split-size %)) filenames))
+  [settings filenames]
+  (mapv #(async/thread (internal-write-zip-from settings %)) filenames))
 
 (defn write-zips-from
   "Keeps on taking elements from channel and writes them to the zipfile.
@@ -93,7 +111,7 @@
     (->>
      (irange 1 writer-threads)
      (map #(str filename-base "_" %))
-     (dispatch-internal-zip-writers transport-channel split-size)
+     (dispatch-internal-zip-writers settings)
      (map async/<!!)
      (doall))
     ))
